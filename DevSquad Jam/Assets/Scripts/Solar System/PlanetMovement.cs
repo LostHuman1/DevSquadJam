76517f0c4a532bfad9c9e1bf615d15715de2a062
LostHuman1 <67455389+LostHuman1@users.noreply.github.com>
2020-12-23 17:18:30 +0545

    Planet Movement

--- DevSquad Jam/Assets/Scripts/Solar System/PlanetMovement.cs
+++ DevSquad Jam/Assets/Scripts/Solar System/PlanetMovement.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class PlanetMovement : MonoBehaviour
 {
     [SerializeField] int speed = 10;
+    [SerializeField] int orbitSpeed = 10; 
     
 
     void Start()
@@ -15,5 +16,6 @@ public class PlanetMovement : MonoBehaviour
     void Update()
     {
         transform.Rotate(0, Time.deltaTime * speed, 0);
+        transform.RotateAround(Vector3.zero, Vector3.up, orbitSpeed * Time.deltaTime);
     }
 }
