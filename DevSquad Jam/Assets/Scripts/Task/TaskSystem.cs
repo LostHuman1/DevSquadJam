8579a822ce48a3fe49ccf4ee8599f003de5ff3cb
DESKTOP-FTIVK0U\Kan <lekcham.kanyanee@gmail.com>
2020-12-29 23:39:29 +0900

    Add description
    add description and edit tassk system

--- DevSquad Jam/Assets/Scripts/Task/TaskSystem.cs
+++ DevSquad Jam/Assets/Scripts/Task/TaskSystem.cs
@@ -4,36 +4,74 @@ using UnityEngine;
 using TMPro;
 public class TaskSystem : MonoBehaviour
 {
-    public TaskSO[] tasks;
+    public enum Planets
+    {
+        GENERIC,
+        MERCURY,
+        VENUS,
+        EARTH,
+        MARS,
+        JUPITER,
+        SATURN,
+        URANUS,
+        NEPTUNE,
+        PLUTO
+    }
 
+    [System.Serializable]
+    public struct Task
+    {
+        public Planets planet;
+        public TaskSO[] tasks;
+    }
+    public Task[] tasks;
     public TextMeshProUGUI objectiveText;
     public TextMeshProUGUI descriptionText;
     public TextMeshProUGUI fuelText;
     public TextMeshProUGUI goldText;
     
-    private int random;
-    private List<int> checkList = new List<int>();
-    
-    public void SetRandomTaskWindow()
+    private int randomTask;
+    private int randomPlanet;
+    public TaskSO previousTask;
+    private Planets previousPlanet;
+    public TaskSO nowTask;
+    private Planets nowPlanet;
+    public List<TaskSO> doneTask = new List<TaskSO>();
+    private void Start()
     {
-        objectiveText.text = tasks[random].objectiveName;
-        descriptionText.text = "  " + tasks[random].description;
-        fuelText.text = tasks[random].fuel.ToString();
-        goldText.text = tasks[random].gold.ToString();
-
+        previousPlanet = (Planets)Random.Range(0, tasks.Length);
+        randomTask  = Random.Range(0, tasks[(int)previousPlanet].tasks.Length);
+        previousTask = tasks[(int)previousPlanet].tasks[randomTask];
+        SetRandomTaskWindow(previousTask);
     }
-    public TaskSO GetRandomTaskSO()
+    public void SetRandomTaskWindow(TaskSO task)
     {
-        while (checkList.Contains(random) && checkList.Count != tasks.Length)
+        objectiveText.text = task.objectiveName;
+        descriptionText.text = "  " + task.description;
+        fuelText.text = task.fuel.ToString();
+        goldText.text = task.gold.ToString();
+        previousPlanet = (Planets)randomPlanet;
+        previousTask = task;
+        nowTask = previousTask;
+        doneTask.Add(task);
+    }
+    public void RandomTaskSO()
+    {
+        RandomPlanet();
+        while (nowTask == previousTask && doneTask.Contains(nowTask))
         {
-            if(tasks.Length == 1)
-            {
-                return tasks[0];
-            }
-            random = UnityEngine.Random.Range(0, tasks.Length);
+            randomTask = Random.Range(0, tasks[randomPlanet].tasks.Length);
+            nowTask = tasks[randomPlanet].tasks[randomTask];
         }
-        checkList.Add(random);
-        SetRandomTaskWindow();
-        return tasks[random];
+        SetRandomTaskWindow(nowTask);
+    }
+
+    public void RandomPlanet()
+    {
+        while (nowPlanet == previousPlanet && tasks.Length != 0)
+        {
+            randomPlanet = Random.Range(0, tasks.Length);
+            nowPlanet = tasks[randomPlanet].planet;
+        }
     }
 }
