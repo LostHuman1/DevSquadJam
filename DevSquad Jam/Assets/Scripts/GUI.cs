d6a6430a4da4524095268556c051a0fec54740bb
DESKTOP-FTIVK0U\Kan <lekcham.kanyanee@gmail.com>
2021-01-04 04:53:12 +0900

    Make volume adjustable in Pause Menu

--- DevSquad Jam/Assets/Scripts/GUI.cs
+++ DevSquad Jam/Assets/Scripts/GUI.cs
@@ -12,15 +12,17 @@ public class GUI : MonoBehaviour
 
 
     public float fuel;
-    [HideInInspector]public float newFuel;
+    [HideInInspector] public float newFuel;
     [SerializeField] TMPro.TMP_Text fuelText;
     public TMPro.TMP_Text errorText;
     float errorTime = 0f;
     public TMPro.TMP_Text planetNameText;
 
-    [SerializeField]GameObject pauseMenu;
+    [SerializeField] GameObject pauseMenu;
     public bool isGamePause = false;
 
+    [SerializeField] private AudioCue buttonAudioCue;
+    [SerializeField] private AudioManager audioManager;
     void Start()
     {
 
@@ -37,7 +39,7 @@ public class GUI : MonoBehaviour
         //Fuel
         fuel = Mathf.Lerp(fuel, newFuel, 5 * Time.deltaTime);
         fuelText.text = "FUEL: " + fuel.ToString("0.00");
-        if(fuel <= 100)
+        if (fuel <= 100)
         {
             fuelText.color = Color.red;
         }
@@ -61,7 +63,7 @@ public class GUI : MonoBehaviour
 
         //error text
         errorTime += Time.deltaTime;
-        if(errorTime >= 2f)
+        if (errorTime >= 2f)
         {
             errorText.text = "";
         }
@@ -72,7 +74,6 @@ public class GUI : MonoBehaviour
         if (pauseMenu.activeSelf) pauseMenu.SetActive(false);
         isGamePause = false;
     }
-
     public void OnPause()
     {
         pauseMenu.SetActive(true);
@@ -83,7 +84,18 @@ public class GUI : MonoBehaviour
     {
         SceneManager.LoadScene("Main Menu");
     }
-
+    public void SetMusicVolume(float volume)
+    {
+        audioManager.SetGroupVolume("BGMVolume", volume);
+    }
+    public void SetSFXVolume(float volume)
+    {
+        audioManager.SetGroupVolume("SFXVolume", volume);
+    }
+    public void PlayButtonSFX()
+    {
+        buttonAudioCue.PlayAudioCue();
+    }
     public void QuitGame()
     {
         Application.Quit();
